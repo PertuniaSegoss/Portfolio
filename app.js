cf706af52c66d305a89adbcc1143a5f609c72e79
Pertunia Lesego Ndhlovu <166070315+PertuniaSegoss@users.noreply.github.com>
2024-10-10 18:52:54 +0200

    Portfolio Files

---
+++ app.js
@@ -0,0 +1,7 @@
+function goToGitHub() {
+  window.location.href = "https://github.com/PertuniaSegoss";
+}
+function goToLinkedIn() {
+  window.location.href =
+    "https://www.linkedin.com/in/pertunia-ndhlovu-384372201/";
+}
