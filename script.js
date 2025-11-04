b57f2afcc458cecbf7ab65b74052eb08387a4c70
Pertunia Lesego Ndhlovu <166070315+PertuniaSegoss@users.noreply.github.com>
2025-11-04 03:11:29 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function goToGitHub() {
+  window.location.href = "https://github.com/PertuniaSegoss";
+}
+
+function goToLinkedIn() {
+  window.location.href =
+    "https://www.linkedin.com/in/pertunia-ndhlovu-384372201/";
+}
