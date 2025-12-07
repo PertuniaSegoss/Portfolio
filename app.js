323ac601dc464754d4982f89783b4688156d6edf
Pertunia Lesego Ndhlovu <166070315+PertuniaSegoss@users.noreply.github.com>
2025-12-08 00:22:11 +0200

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-function goToGitHub() {
-  window.location.href = "https://github.com/PertuniaSegoss";
-}
-function goToLinkedIn() {
-  window.location.href =
-    "https://www.linkedin.com/in/pertunia-ndhlovu-384372201/";
-}
